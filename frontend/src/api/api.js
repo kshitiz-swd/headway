130a23a72cc5c05f523c5d2d5d9ad443970ec28c
kshitiz-swd <kshitiz123srivastava@gmail.com>
2026-04-15 03:10:08 +0530

    connected

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: 'http://localhost:8000/api',
+    baseURL: 'https://headway-jufc.onrender.com/api',
     withCredentials: true
 })
 
