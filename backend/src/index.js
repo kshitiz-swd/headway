fd3e1eb30f89052baf0f53eb039a8f831a64644c
kshitiz-swd <kshitiz123srivastava@gmail.com>
2026-04-15 03:28:40 +0530

    fixed cors and connected

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000
 
 const app = express()
 app.use(express.json())
-app.use(cors({ origin: ["http://localhost:5173", "https://headway-mocha.vercel.app/auth"], credentials: true }))
+app.use(cors({ origin: ["http://localhost:5173", "https://headway-mocha.vercel.app"], credentials: true }))
 app.use(cookieParser())
 
 app.use('/api/auth', authRoutes)
